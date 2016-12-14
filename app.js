080dd73bff7b303a1f069f4cba6ecaa2f782c73c
jonfranc <jmtf629@gmail.com>
2016-12-13 18:20:40 -0800

    Update app.js

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ var intents = new builder.IntentDialog({ recognizers: [recognizer] })
         Bing.composite(session.message.text, {
         top: 5
         }, function(error, res, body){
-            session.send('Here are some search results: %s', (body.news).toString());
+            session.send('Here are some search results: %s', JSON.stringify(body.news));
         });
     });
 
