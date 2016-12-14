f5324224272eee3682ee666fcac30dee2077885a
Jon Francis <jmtf629@gmail.com>
2016-12-13 16:13:02 -0800

    hardocding the url

--- app.js
+++ app.js
@@ -9,26 +9,27 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 // Create chat bot
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID, 
+    appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
-const LuisModelUrl = process.env.LUIS_URL;
+//const LuisModelUrl = process.env.LUIS_URL;
 
 // Main dialog with LUIS
-var recognizer = new builder.LuisRecognizer(LuisModelUrl);
+//var recognizer = new builder.LuisRecognizer(LuisModelUrl);
+var recognizer = new builder.LuisRecognizer('https://iswudev.azure-api.net/luis/v2.0/apps/98eead94-8470-4337-9280-5bb7d5fb8502?subscription-key=c2cd164e833947fbb41ae9a3d9886a1f&q=');
 var intents = new builder.IntentDialog({ recognizers: [recognizer] })
     .matches('sendCall', [
         function (session, args, next) {
-            
+
             var contact = builder.EntityRecognizer.findEntity(args.entities, 'emergency contact');
             var name = builder.EntityRecognizer.findEntity(args.entities, 'name');
             var encyclopedia = builder.EntityRecognizer.findEntity(args.entities, 'encyclopedia');
-          
+
             var msg = "";
-            
+
             if (contact) {
                 msg += "I think your contact is: "+contact.entity+"\n";
             }
@@ -39,7 +40,7 @@ var intents = new builder.IntentDialog({ recognizers: [recognizer] })
                 msg += "I think your encyclopedia is: "+encyclopedia.entity+"\n";
             }
 
-                
+
             session.send('sendCall triggered: \'%s\'\n\n%s', session.message.text,msg);
         }
     ])
@@ -52,7 +53,7 @@ var intents = new builder.IntentDialog({ recognizers: [recognizer] })
             var location = builder.EntityRecognizer.findEntity(args.entities, 'location');
 
             var msg = "";
-            
+
             if (intensity) {
                 msg += "I think your intensity is: "+intensity.entity+"\n";
             }
